599413bdba758fc5301c1dbc09dea623c67dcb9f
Глеб Боушев <core@4c74356b41.com>
2016-03-02 17:26:15 +0300

    First Version, done. Planned steps: work on forecasting a bit (perhaps); fix the icons, maybe link to a live tile.

---
+++ UWP-04/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace UWP_04
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var position = await LocationManager.GetPosition();
+
+                OpenWeatherMapProxyForecast.RootObjectForecast myWeatherForecast =
+                    await OpenWeatherMapProxyForecast.GetWeatherForecast(position.Coordinate.Latitude, position.Coordinate.Longitude);
+
+                string icon = String.Format("ms-appx:///Assets/{0}.png", myWeatherForecast.list[0].weather[0].icon);
+                ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+                City.Text = myWeatherForecast.city.name;
+                Celcius.Text = "Temperatura: °" + ((int)myWeatherForecast.list[0].main.temp).ToString();
+                Weather.Text = "Como hace hoy: " + myWeatherForecast.list[0].weather[0].description;
+
+                TomorrowTemp.Text = ((int)myWeatherForecast.list[8].main.temp).ToString();
+                TomorrowMain.Text = myWeatherForecast.list[8].weather[0].main;
+
+                AfterTomorrowTemp.Text = ((int)myWeatherForecast.list[16].main.temp).ToString();
+                AfterTomorrowMain.Text = myWeatherForecast.list[16].weather[0].main;
+
+                AfterAfterTomorrowTemp.Text = ((int)myWeatherForecast.list[24].main.temp).ToString();
+                AfterAfterTomorrowMain.Text = myWeatherForecast.list[24].weather[0].main;
+            }
+            catch
+            {
+                Weather.Text = "Unable to get weather at this time";
+            }
+        }
+    }
+}
---
+++ UWP-04/OpenWeatherMapProxyForecast.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_04
+{
+    class OpenWeatherMapProxyForecast
+    {
+        public async static Task<RootObjectForecast> GetWeatherForecast(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=5cac47538f90d19879ecaa7c8c7fab67&units=metric", lat, lon);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObjectForecast));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObjectForecast)serializer.ReadObject(ms);
+
+            return data;
+        }
+
+        [DataContract]
+        public class CoordForecast
+        {
+            [DataMember]
+            public double lon { get; set; }
+            [DataMember]
+            public double lat { get; set; }
+        }
+        [DataContract]
+        public class SysForecast
+        {
+            [DataMember]
+            public int population { get; set; }
+        }
+        [DataContract]
+        public class CityForecast
+        {
+            [DataMember]
+            public int id { get; set; }
+            [DataMember]
+            public string name { get; set; }
+            [DataMember]
+            public CoordForecast coord { get; set; }
+            [DataMember]
+            public string country { get; set; }
+            [DataMember]
+            public int population { get; set; }
+            [DataMember]
+            public SysForecast sys { get; set; }
+        }
+        [DataContract]
+        public class MainForecast
+        {
+            [DataMember]
+            public double temp { get; set; }
+            [DataMember]
+            public double temp_min { get; set; }
+            [DataMember]
+            public double temp_max { get; set; }
+            [DataMember]
+            public double pressure { get; set; }
+            [DataMember]
+            public double sea_level { get; set; }
+            [DataMember]
+            public double grnd_level { get; set; }
+            [DataMember]
+            public int humidity { get; set; }
+            [DataMember]
+            public double temp_kf { get; set; }
+        }
+        [DataContract]
+        public class WeatherForecast
+        {
+            [DataMember]
+            public int id { get; set; }
+            [DataMember]
+            public string main { get; set; }
+            [DataMember]
+            public string description { get; set; }
+            [DataMember]
+            public string icon { get; set; }
+        }
+        [DataContract]
+        public class ListForecast
+        {
+            [DataMember]
+            public MainForecast main { get; set; }
+            [DataMember]
+            public List<WeatherForecast> weather { get; set; }
+            [DataMember]
+            public string dt_txt { get; set; }
+        }
+        [DataContract]
+        public class RootObjectForecast
+        {
+            [DataMember]
+            public CityForecast city { get; set; }
+            [DataMember]
+            public List<ListForecast> list { get; set; }
+        }
+    }
+}
